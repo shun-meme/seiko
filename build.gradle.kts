40041273ad38fa217dd4d366aa40cfa02fea6ca2
Songpol Anannetikul <1527535+songpola@users.noreply.github.com>
2024-10-31 17:44:57 +0700

    fix: 🐛 Merge with the main branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,20 @@ dependencies {
 
     implementation("com.formdev:flatlaf:3.5.1")
     implementation("com.formdev:flatlaf-intellij-themes:3.5.1")
+
     implementation("com.opencsv:opencsv:5.5.2")
+    constraints {
+        implementation("org.apache.commons:commons-text:1.10.0") {
+            because(
+                """
+                Dependency maven:org.apache.commons:commons-text:1.9 is vulnerable
+                Upgrade to 1.10.0
+                GHSA-599f-7c49-w659,  Score: 9.8
+                Read More: https://osv.dev/vulnerability/GHSA-599f-7c49-w659
+                """.trimIndent()
+            )
+        }
+    }
 }
 
 application {
--- src/main/java/dev/songpola/seiko/App.java
+++ src/main/java/dev/songpola/seiko/App.java
@@ -1,19 +1,82 @@
 package dev.songpola.seiko;
 
 import com.formdev.flatlaf.IntelliJTheme;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 import dev.songpola.seiko.task.TaskManagerController;
 import dev.songpola.seiko.task.model.TaskListModel;
+import dev.songpola.seiko.task.model.TaskModel;
 import dev.songpola.seiko.timer.PomodoroTimerController;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class App extends JFrame {
+    private static final String USER_HOME_PATH = System.getProperty("user.home");
+    private static final String TASK_DATA_FILE_NAME = "tasks.csv";
+
     private final TaskListModel taskListModel = new TaskListModel();
 
     public App(String title) {
         super(title);
+        initState();
         setup();
-        addTabs();
+        setupTabs();
+    }
+
+    private void initState() {
+        var path = Path.of(USER_HOME_PATH, TASK_DATA_FILE_NAME);
+        loadTasksFromFile(path);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                // Save tasks to file on close
+                saveTasksToFile(path);
+                e.getWindow().dispose();
+            }
+        });
+    }
+
+    private void loadTasksFromFile(Path path) {
+        try (var reader = new CSVReader(new FileReader(path.toString()))) {
+            var lines = reader.readAll();
+            for (var line : lines) {
+                String title = line[0];
+                boolean completed = Boolean.parseBoolean(line[1]);
+                taskListModel.addTask(new TaskModel(title, completed));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("No existing tasks file found. Creating new tasks file: " + path);
+            saveTasksToFile(path);
+        } catch (IOException | CsvException e) {
+            // Unable to read tasks from file
+            // TODO: Show dialog to user
+        }
+    }
+
+    private void saveTasksToFile(Path path) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(path.toString()))) {
+            var tasks = taskListModel.toArray();
+            for (Object task : tasks) {
+                var taskModel = (TaskModel) task;
+                String[] line = {
+                    taskModel.getTitle(),
+                    String.valueOf(taskModel.isCompleted())
+                };
+                writer.writeNext(line);
+            }
+        } catch (IOException e) {
+            // Unable to write tasks to file
+            // TODO: Show dialog to user
+        }
     }
 
     private void setup() {
@@ -27,11 +90,10 @@ public class App extends JFrame {
         setLocationRelativeTo(null);        // setLocationByPlatform(true);
     }
 
-    private void addTabs() {
+    private void setupTabs() {
         JTabbedPane tabs = new JTabbedPane();
         tabs.addTab("Tasks", new TaskManagerController(taskListModel));
         tabs.addTab("Timer", new PomodoroTimerController(taskListModel));
-        tabs.setSelectedIndex(1);
         add(tabs);
     }
 }
